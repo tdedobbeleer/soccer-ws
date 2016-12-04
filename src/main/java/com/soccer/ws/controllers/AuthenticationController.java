ea020afc6a12cf5f4f6361ad36e07fb4367ec23b
Tom De Dobbeleer <tom.dedobbeleer@kuleuven.be>
2016-12-04 10:46:00 +0100

    added is authenticated method

--- src/main/java/com/soccer/ws/controllers/AuthenticationController.java
+++ src/main/java/com/soccer/ws/controllers/AuthenticationController.java
@@ -1,11 +1,13 @@
 package com.soccer.ws.controllers;
-;
-import com.soccer.ws.persistence.UserDetailsAdapter;
-import com.soccer.ws.security.TokenUtils;
+
 import com.soccer.ws.dto.AuthenticationRequestDTO;
 import com.soccer.ws.dto.AuthenticationResponseDTO;
+import com.soccer.ws.persistence.UserDetailsAdapter;
+import com.soccer.ws.security.TokenUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,7 +19,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
@@ -44,28 +45,25 @@ public class AuthenticationController extends AbstractRestController {
   @RequestMapping(value = "/auth", method = RequestMethod.POST)
   @ResponseBody
   @ApiOperation(value = "authenticate", nickname = "authenticate")
+  @ApiResponses(value = {
+          @ApiResponse(code = 401, message = "Access denied")})
   public ResponseEntity<?> authenticationRequest(@RequestBody AuthenticationRequestDTO authenticationRequestDTO, Device device) throws AuthenticationException {
-    try {
-      // Perform the authentication
-      Authentication authentication = this.authenticationManager.authenticate(
-              new UsernamePasswordAuthenticationToken(
-                      authenticationRequestDTO.getUsername(),
-                      authenticationRequestDTO.getPassword()
-              )
-      );
-      SecurityContextHolder.getContext().setAuthentication(authentication);
+    // Perform the authentication
+    Authentication authentication = this.authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                    authenticationRequestDTO.getUsername(),
+                    authenticationRequestDTO.getPassword()
+            )
+    );
+    SecurityContextHolder.getContext().setAuthentication(authentication);
 
-      // Reload password post-authentication so we can generate token
-      UserDetailsAdapter user = (UserDetailsAdapter) this.userDetailsService.loadUserByUsername(authenticationRequestDTO.getUsername());
-      String token = this.tokenUtils.generateToken(user, device);
+    // Reload password post-authentication so we can generate token
+    UserDetailsAdapter user = (UserDetailsAdapter) this.userDetailsService.loadUserByUsername(authenticationRequestDTO.getUsername());
+    String token = this.tokenUtils.generateToken(user, device);
 
-      // Return the token
-      return ResponseEntity.ok(new AuthenticationResponseDTO(token, user.getFirstName(), user.getLastName(), user.getUsername()));
-    }
-    catch (Exception e) {
-      logger.warn(String.format("Could not log in user %s", authenticationRequestDTO.getUsername()));
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
+    // Return the token
+    return ResponseEntity.ok(new AuthenticationResponseDTO(token, user.getFirstName(), user.getLastName(), user.getUsername()));
+
   }
 
   @RequestMapping(value = "/auth/refresh", method = RequestMethod.GET)
@@ -82,5 +80,13 @@ public class AuthenticationController extends AbstractRestController {
       return ResponseEntity.badRequest().body(null);
     }
   }
+
+  @RequestMapping(value = "/auth/isFullyAuthenticated", method = RequestMethod.GET)
+  @ResponseBody
+  @ApiOperation(value = "Is fully authenticated", nickname = "isFullyAuthenticated")
+  public ResponseEntity<Boolean> isFullyAuthenticated() {
+    return new ResponseEntity<>(getAccountFromSecurity() != null, HttpStatus.OK);
+  }
+
 
 }
