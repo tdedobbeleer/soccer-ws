63ac67da19e806956cd41ca2494f2a94e90901f3
u0090265 <tom.dedobbeleer@kuleuven.be>
2017-08-28 13:54:01 +0200

    keep heroku from idling during office hours

--- src/main/java/com/soccer/ws/tasks/WakeUpTask.java
+++ src/main/java/com/soccer/ws/tasks/WakeUpTask.java
@@ -10,13 +10,13 @@ import org.springframework.web.client.RestTemplate;
  * Created by u0090265 on 28.08.17.
  */
 public class WakeUpTask {
-    private static final Logger LOG = LoggerFactory.getLogger(WakeUpTask.class);
-    @Value("$base.url")
+    private static final Logger log = LoggerFactory.getLogger(WakeUpTask.class);
+    @Value("${base.url}")
     private String baseUrl;
 
-    @Scheduled(cron = "0 */10 7-23 * * *", zone = "Europe/Brussels")
-    public void herokuNotIdle() {
-        LOG.debug("Heroku not idle execution");
+    @Scheduled(cron = "0 */1 7-23 * * *", zone = "Europe/Brussels")
+    public void notIdle() {
+        log.info("Making sure application is not idle...");
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getForObject(baseUrl + "/health", Object.class);
     }
