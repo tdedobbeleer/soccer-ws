a5faf01f29397e5d1ecc5d9cf73130baedd2d753
u0090265 <tom.dedobbeleer@kuleuven.be>
2017-08-30 17:50:39 +0200

    kill the task

--- src/main/java/com/soccer/ws/tasks/WakeUpTask.java
+++ src/main/java/com/soccer/ws/tasks/WakeUpTask.java
@@ -4,13 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by u0090265 on 28.08.17.
  */
-@Component
+//In order to keep the app awake, we need an external job to poll the application.
+//@Component
 public class WakeUpTask implements Task {
     private static final Logger log = LoggerFactory.getLogger(WakeUpTask.class);
     @Value("${application.url}")
