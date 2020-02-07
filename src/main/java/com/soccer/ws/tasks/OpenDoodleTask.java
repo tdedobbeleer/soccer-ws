8a6e315ab8cf3dbb1216d00d3f632ae694e505b6
Tom <tdedobbeleer@gmail.com>
2020-02-07 09:05:31 +0100

    only send email when needed

--- src/main/java/com/soccer/ws/tasks/OpenDoodleTask.java
+++ src/main/java/com/soccer/ws/tasks/OpenDoodleTask.java
@@ -24,15 +24,17 @@ public class OpenDoodleTask implements Task {
     private final MailService mailService;
     private final String baseUrl;
     private final MessageSource messageSource;
+    private final boolean sendDoodleOpenEmail;
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    public OpenDoodleTask(MatchesService matchesService, AccountService accountService, MailService mailService, @Value("${base.url}") String baseUrl, MessageSource messageSource) {
+    public OpenDoodleTask(MatchesService matchesService, AccountService accountService, MailService mailService, @Value("${base.url}") String baseUrl, MessageSource messageSource, @Value("${matches.doodle.open.email}") boolean sendDoodleOpenEmail) {
         this.matchesService = matchesService;
         this.accountService = accountService;
         this.mailService = mailService;
         this.messageSource = messageSource;
         this.baseUrl = baseUrl;
+        this.sendDoodleOpenEmail = sendDoodleOpenEmail;
     }
 
     @Override
@@ -40,15 +42,19 @@ public class OpenDoodleTask implements Task {
     public void execute() {
         log.info("Execute OpenDoodleTask - start");
         matchesService.openNextMatchDoodle().forEach(m -> {
-            log.debug("Match {} is opened, sending emails.", m.getDescription());
-            String subject = messageSource.getMessage("email.doodle.open.subject", new String[]{m
-                    .getDescription(), m.getStringDateTime()}, Locale.ENGLISH);
-            accountService.getAllActivateAccounts().forEach(a -> {
-                log.debug("Sending doodle open email to {}.", a.getUsername());
-                mailService.sendMail(a.getUsername(), a.toString(), subject, MailTypeEnum
-                        .DOODLE_OPEN, ImmutableMap.of(EMAIL_ACCOUNT_VARIABLE, a,
-                        EMAIL_BASE_URL_VARIABLE, baseUrl));
-            });
+            //only send email when prop is set
+            if (sendDoodleOpenEmail) {
+                log.debug("Match {} is opened, sending emails.", m.getDescription());
+                String subject = messageSource.getMessage("email.doodle.open.subject", new String[]{m
+                        .getDescription(), m.getStringDateTime()}, Locale.ENGLISH);
+                accountService.getAllActivateAccounts().forEach(a -> {
+                    log.debug("Sending doodle open email to {}.", a.getUsername());
+                    mailService.sendMail(a.getUsername(), a.toString(), subject, MailTypeEnum
+                            .DOODLE_OPEN, ImmutableMap.of(EMAIL_ACCOUNT_VARIABLE, a,
+                            EMAIL_BASE_URL_VARIABLE, baseUrl));
+
+                });
+            }
         });
         log.info("Execute OpenDoodleTask - end");
     }
