cb6840bb1380b162ec960e2a85b23b457cf1c539
Tom De Dobbeleer <tom.dedobbeleer@kuleuven.be>
2020-06-29 09:42:27 +0200

    lose local pw encoder

--- src/main/java/com/soccer/ws/configuration/PasswordEncoderConfig.java
+++ src/main/java/com/soccer/ws/configuration/PasswordEncoderConfig.java
@@ -6,11 +6,9 @@ package com.soccer.ws.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@Profile("!default")
 public class PasswordEncoderConfig {
 
     @Bean
