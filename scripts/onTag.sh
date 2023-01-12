71e6100acf0280521d7d0b10fc0f98410bcd5389
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-12 16:41:57 +0100

    new travis command + one liner

--- scripts/onTag.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ ! -z "$TRAVIS_TAG" ]
-then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings "${TRAVIS_BUILD_DIR}/.travis/mvn-settings.xml" org.codehaus.mojo:versions-maven-plugin:2.14.2:set -DnewVersion=$TRAVIS_TAG
-else
-    echo "not on a tag -> keep snapshot version in pom.xml"
-fi
