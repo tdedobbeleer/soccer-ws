1a7391a183504617bcb5546651a296c76c5e9425
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-14 11:33:22 +0100

    make sure travis.com is used

--- scripts/trigger_travis_deploy.sh
+++ scripts/trigger_travis_deploy.sh
@@ -1,7 +1,7 @@
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST TRAVIS_TAG=$TRAVIS_TAG"
 if [[ -n "$TRAVIS_TAG" ]] ; then
   curl -LO --retry 3 https://raw.githubusercontent.com/plume-lib/trigger-travis/master/trigger-travis.sh
-  sh trigger-travis.sh tdedobbeleer soccer-ws-docker $TRAVIS_ACCESS_TOKEN
+  sh trigger-travis.sh --pro tdedobbeleer soccer-ws-docker $TRAVIS_ACCESS_TOKEN
 else
 echo "trigger-travis.sh did not run"
 fi
